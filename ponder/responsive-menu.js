7e5b46486054a19611361ff9eeba638accd2507b
IHeredia-53 <herediaisaac53@gmail.com>
2026-05-14 19:22:25 -0600

    lots of changes

---
+++ ponder/responsive-menu.js
@@ -0,0 +1,18 @@
+let menuButton = document.getElementsByClassName("menu-btn")[0];
+
+console.log(menuButton);
+
+// event listener
+menuButton.addEventListener("click", handleMenuButtonClick);
+
+function handleMenuButtonClick(event) {
+    console.log(event);
+    // toggle on/off the menu display
+
+    //grab the nav from the DOM
+    let nav = document.querySelector("nav");
+    //toggle the .hide class on/off
+    nav.classList.toggle("unhide");
+    //profit
+    menuButton.classList.toggle("change");
+}
